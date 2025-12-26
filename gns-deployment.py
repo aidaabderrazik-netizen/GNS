3fae95a5e950b654e598b38bcb862dc6efe78adc
liliabou31 <liliaboubaker15@gmail.com>
2025-12-26 20:04:20 +0300

    update

--- gns-deployment.py
+++ gns-deployment.py
@@ -4,11 +4,12 @@ import os
 OSPF_PID = 1
 RIP_NAME = "RIP-ASX"
 
+# Load routers JSON
 with open("routers.json") as f:
     routers = json.load(f)
 
 for r in routers:
-    gns_path = r["gns_path"]
+    gns_path = r.get("gns_path", ".")  # fallback if not defined
     startup = os.path.join(gns_path, "i1_startup-config.cfg")
 
     if not os.path.isdir(gns_path):
@@ -17,18 +18,18 @@ for r in routers:
 
     with open(startup, "w") as cfg:
         # -------------------------
-        # Base
+        # Base config
         # -------------------------
         cfg.write(f"hostname {r['name']}\n")
         cfg.write("ipv6 unicast-routing\n\n")
 
         # -------------------------
-        # Loopback
+        # Loopback interface
         # -------------------------
         cfg.write("interface Loopback0\n")
         cfg.write(f" ipv6 address {r['loopback']}\n")
 
-        if r["IGP"] == "OSPF":
+        if r["routing"]["igp"] == "OSPF":
             cfg.write(f" ipv6 ospf {OSPF_PID} area 0\n")
         else:
             cfg.write(f" ipv6 rip {RIP_NAME} enable\n")
@@ -36,56 +37,53 @@ for r in routers:
         cfg.write("exit\n\n")
 
         # -------------------------
-        # Interfaces
+        # Physical / internal / eBGP interfaces
         # -------------------------
         for idx, iface in enumerate(r["interfaces"]):
             ifname = f"GigabitEthernet0/{idx}"
-
             cfg.write(f"interface {ifname}\n")
             cfg.write(f" ipv6 address {iface['local_ip']}\n")
             cfg.write(" no shutdown\n")
 
-            if r["IGP"] == "OSPF":
+            if r["routing"]["igp"] == "OSPF" and iface.get("type") != "ebgp":
                 cfg.write(f" ipv6 ospf {OSPF_PID} area 0\n")
-            else:
+            elif r["routing"]["igp"] == "RIP" and iface.get("type") != "ebgp":
                 cfg.write(f" ipv6 rip {RIP_NAME} enable\n")
 
             cfg.write("exit\n\n")
 
         # -------------------------
-        # OSPF process (AS Y)
+        # IGP process
         # -------------------------
-        if r["IGP"] == "OSPF":
+        if r["routing"]["igp"] == "OSPF":
             cfg.write(f"ipv6 router ospf {OSPF_PID}\n")
             cfg.write(f" router-id {r['router_id_bgp']}\n")
             cfg.write("exit\n\n")
-
-        # -------------------------
-        # RIP process (AS X)
-        # -------------------------
-        if r["IGP"] == "RIP":
+        elif r["routing"]["igp"] == "RIP":
             cfg.write(f"ipv6 router rip {RIP_NAME}\n")
             cfg.write("exit\n\n")
 
         # -------------------------
-        # BGP
+        # BGP process
         # -------------------------
         cfg.write(f"router bgp {r['as_number']}\n")
         cfg.write(f" bgp router-id {r['router_id_bgp']}\n")
         cfg.write(" no bgp default ipv4-unicast\n")
 
-        # iBGP
-        for peer in r["iBGP_peers"]:
+        # iBGP peers
+        for peer in r["routing"]["ibgp_peers"]:
             peer_id = next(x["loopback"] for x in routers if x["name"] == peer)
             cfg.write(f" neighbor {peer_id.split('/')[0]} remote-as {r['as_number']}\n")
             cfg.write(" address-family ipv6\n")
             cfg.write(f"  neighbor {peer_id.split('/')[0]} activate\n")
             cfg.write(" exit-address-family\n")
 
-        # eBGP
-        for peer in r["eBGP_peers"]:
+        # eBGP peers
+        for peer in r["routing"]["ebgp_peers"]:
             peer_router = next(x for x in routers if x["name"] == peer["peer"])
-            peer_ip = peer_router["interfaces"][0]["local_ip"].split("/")[0]
+            # Find the interface connected to this eBGP peer
+            iface = next(i for i in r["interfaces"] if i["peer"] == peer["peer"] and i.get("type") == "ebgp")
+            peer_ip = iface["peer_ip"].split("/")[0]
 
             cfg.write(f" neighbor {peer_ip} remote-as {peer['peer_as']}\n")
             cfg.write(" address-family ipv6\n")
